40b38257a28bc24fb7fa3bfc29e097ed1d00d885
Paul D'Ambra <paul.damba@coop.co.uk>
2021-05-22 14:06:20 +0100

    simplify userpool class

--- IdentityProvider/UserPool.py
+++ IdentityProvider/UserPool.py
@@ -8,14 +8,9 @@ class FailedLogin:
 
 
 class UserPool(object):
-    _instance = None
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(UserPool, cls).__new__(cls)
-        return cls._instance
-
-    def login(self, username, password):
+    @staticmethod
+    def login(username, password):
         if username == "valid" or password == "valid":
             return SuccessfulLogin()
         else:
--- basic state machine/tests/test_user_pool.py
+++ basic state machine/tests/test_user_pool.py
@@ -6,13 +6,13 @@ from IdentityProvider.UserPool import UserPool, SuccessfulLogin, FailedLogin
 class TestUserPool(unittest.TestCase):
 
     def test_can_login_when_username_is_the_string_valid(self):
-        login_result = UserPool().login("valid", "anything else")
+        login_result = UserPool.login("valid", "anything else")
         self.assertIsInstance(login_result, SuccessfulLogin)
 
     def test_can_login_when_password_is_the_string_valid(self):
-        login_result = UserPool().login("anything else", "valid")
+        login_result = UserPool.login("anything else", "valid")
         self.assertIsInstance(login_result, SuccessfulLogin)
 
     def test_cannot_login_when_neither_username_nor_password_is_the_string_valid(self):
-        login_result = UserPool().login("anything else", "anything else")
+        login_result = UserPool.login("anything else", "anything else")
         self.assertIsInstance(login_result, FailedLogin)
